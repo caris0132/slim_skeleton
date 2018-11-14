8f06aa80397b9411446b306f75d4513665ddbc4c
caris0132 <nguyenductri1h1@gmail.com>
2018-11-14 17:07:05 +0700

    feature admin

--- src/routes.php
+++ src/routes.php
@@ -9,8 +9,29 @@ $app->get('/home/[{name}]', 'Src\Controllers\HomeController:home');
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+	$this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+	return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->group('/admin', function ()
+{
+	$this->group('/news', function ()
+	{
+		$this->get('/', 'Src\Controllers\HomeController:index');
+		$this->post('/', 'Src\Controllers\HomeController:store');
+		$this->get('/:id', 'Src\Controllers\HomeController:show');
+		$this->put('/:id', 'Src\Controllers\HomeController:store');
+		$this->delete('/:id', 'Src\Controllers\HomeController:home');
+
+	});
+	$this->get('/{name}', function (Request $request, Response $response, array $args) {
+    // Sample log message
+		$this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+		return $this->renderer->render($response, 'admin/index.php', $args);
+	});
+});
+
